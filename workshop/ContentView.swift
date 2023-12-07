37af73180793a96d8141937e38eab460fe306609
julio2505 <juliomeza2510@outlook.com>
2023-12-06 19:20:32 -0600

    Initial Commit

---
+++ workshop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  workshop
+//
+//  Created by Alumno on 06/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ workshop/workshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  workshopApp.swift
+//  workshop
+//
+//  Created by Alumno on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct workshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
