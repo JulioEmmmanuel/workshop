60ea5bfc6ba11309c39d54a685a891355cfa5bfc
julio2505 <juliomeza2510@outlook.com>
2023-12-07 15:53:03 -0600

    Proyecto de ML

---
+++ workshop/Classifier.swift
@@ -0,0 +1,39 @@
+//
+//  Classifier.swift
+//  workshop
+//
+//  Created by Alumno on 06/12/23.
+//
+
+import CoreML
+import Vision
+import CoreImage
+
+struct Classifier {
+    
+    private(set) var results: String?
+    
+    mutating func detect(ciImage: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: MobileNetV2(configuration: MLModelConfiguration()).model)
+        else {
+            return
+        }
+        
+        let request = VNCoreMLRequest(model: model)
+        
+        let handler = VNImageRequestHandler(ciImage: ciImage, options: [:])
+        
+        try? handler.perform([request])
+        
+        guard let results = request.results as? [VNClassificationObservation] else {
+            return
+        }
+        
+        if let firstResult = results.first {
+            self.results = firstResult.identifier
+        }
+        
+    }
+    
+}
---
+++ workshop/ImageClassifier.swift
@@ -0,0 +1,24 @@
+//
+//  ImageClassifier.swift
+//  workshop
+//
+//  Created by Alumno on 07/12/23.
+//
+
+import SwiftUI
+
+class ImageClassifier: ObservableObject {
+    
+    @Published private var classifier = Classifier()
+    
+    var imageClass: String? {
+        classifier.results
+    }
+        
+    func detect(uiImage: UIImage) {
+        guard let ciImage = CIImage (image: uiImage) else { return }
+        classifier.detect(ciImage: ciImage)
+        
+    }
+        
+}
